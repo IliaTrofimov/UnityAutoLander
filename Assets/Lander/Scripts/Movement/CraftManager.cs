1ddafc58fa4732f6a00ef8f3aae0b27e838dbcf2
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2023-04-03 03:15:27 +0300

    craft manager class

---
+++ Assets/Lander/Scripts/Movement/CraftManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+
+using UnityEngine;
+
+using Shared;
+using Thrusters;
+using CraftState;
+using ProximitySensors;
+using Telemetry;
+
+namespace Movement
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Rigidbody), typeof(ThrustersController))]
+    public class CraftManager : MonoBehaviour
+    {
+        [Range(0.1f, 10)]
+        public float MaxMovementAfterTouch = 5;
+
+        [Range(2, 60)]
+        public float MaxRotationAfterTouch = 20;
+
+        [Range(0, 60)]
+        public float MaxTocuhVelocity = 40;
+
+        [Range(0.01f, 10)]
+        public float StandStillSeconds = 2;
+
+        private BaseState state;
+        private Rigidbody body;
+        private HeightSensor heightSensor;
+        private ProximitySensor[] directionalSensors;
+        private Thruster[] thrusters;
+        private ThrustersController thrustersController;
+        private ConsoleTelemetryLogger logger;
+        private bool isCollided;
+
+        private MovementInfo Movement => new MovementInfo()
+        {
+            Position = body.worldCenterOfMass,
+            Velocity = body.velocity,
+            EulerAngles = body.transform.eulerAngles,
+            AngularVelocity = body.angularVelocity,
+            Height = heightSensor.Distance,
+            IsCollided = isCollided
+        };
+
+
+        private void Start()
+        {
+            FlyingState.MaxTocuhVelocity = MaxTocuhVelocity;
+            FixationState.MaxMovementAfterTouch = MaxMovementAfterTouch;
+            FixationState.MaxRotationAfterTouch = MaxRotationAfterTouch;
+            FixationState.StandStillSeconds = StandStillSeconds;
+
+            body = gameObject.GetComponent<Rigidbody>();
+            heightSensor = gameObject.GetComponentInChildren<HeightSensor>();
+            directionalSensors = gameObject.GetComponentsInChildren<ProximitySensor>();
+            thrustersController = gameObject.GetComponent<ThrustersController>();
+            thrusters = gameObject.GetComponentsInChildren<Thruster>();
+
+            state = new FlyingState(Movement);
+
+            try
+            {
+                logger = gameObject.GetComponent<ConsoleTelemetryLogger>();
+            }
+            catch
+            {
+                logger = null;      
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            var movement = Movement;
+            state = state.NextState(movement);
+            if (state.IsStateChanged)
+                Debug.Log(state);
+
+            if (logger != null)
+            {
+                logger.SetMovement(movement).SetFuel(thrustersController.Fuel);
+                foreach (var s in directionalSensors)
+                    logger.SetSensorsData(s.Position, s.Distance);
+                foreach (var t in thrusters)
+                    logger.SetThrustData(t.Position, 0);
+            }
+        }
+
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            isCollided = true;
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            isCollided = false;
+        }
+    }
+}
--- Assets/Lander/Scripts/Movement/Movement.cs
+++ Assets/Lander/Scripts/Movement/Movement.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
-using System.Collections;
+﻿using System.Collections;
 using System.Linq;
 
+using UnityEngine;
+using UnityEngine.Events;
+
+using Shared;
 using Thrusters;
-using Utils;
 using CraftState;
 
-namespace Lander
+namespace Movement
 {
-
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(ThrustersController), typeof(Rigidbody))]
     public class Movement : MonoBehaviour
     {
         [Range(1, 50)]
@@ -24,27 +26,15 @@ namespace Lander
         [Range(1, 250)]
         public float MainThrust = 100;
 
-        public bool DrawVelocityVector = true;
-        public bool DrawCenterOfMass = true;
-
-        public UnityEvent<MovementInfo> ObjectMovedEvent;
-
-        private ThrustersController thrustersController;
         private BaseState state;
-        private Rigidbody body;
         private bool isCollided;
+        private ThrustersController thrustersController;
+        private Rigidbody body;
 
         private void Start()
         {
             body = gameObject.GetComponent<Rigidbody>();
-            thrustersController = new ThrustersController(GetComponentsInChildren<Thruster>());
-            state = new FlyingState(new MovementInfo() { });
-        }
-
-        private void Update()
-        {
-            if (DrawVelocityVector)
-                Debug.DrawLine(body.position, body.position + body.velocity.normalized*5, Color.yellow);
+            thrustersController = gameObject.GetComponent<ThrustersController>();
         }
 
 
@@ -55,43 +45,12 @@ namespace Lander
             float yaw = Input.GetAxis("Yaw") * YawThrust;
             float up = Input.GetAxis("Jump") * MainThrust;
 
-            thrustersController.ApplyMovement(0, up, 0, roll, 0, pitch);
-
-            var temp = state.NextState(GetMovementInfo());
-            if (temp.GetType() != state.GetType())
-                Debug.Log($"{state} -> {temp}");
-            state = temp;
+            thrustersController.ApplyMovement(0, up, 0, roll, yaw, pitch);
 
             if (Input.GetKey(KeyCode.LeftShift))
                 body.angularVelocity = new Vector3();
             if (Input.GetKey(KeyCode.LeftControl))
                 body.velocity = new Vector3();
         }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            isCollided = true;
-        }
-
-        private void OnCollisionExit(Collision collision)
-        {
-            isCollided = false;
-        }
-
-
-
-        private MovementInfo GetMovementInfo()
-        {
-            return new MovementInfo()
-            {
-                Position = body.worldCenterOfMass,
-                Velocity = body.velocity,
-                EulerAngles = body.transform.eulerAngles,
-                AngularVelocity = body.angularVelocity,
-                Height = 0,
-                IsCollided = isCollided
-            };
-        }
     }
-
 }
