aaea9bb2f2a3c0c895a90ff79fd9fd47e199b10d
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2023-04-16 18:09:33 +0300

    moved ai

---
+++ Assets/Lander/Scripts/AI/AutoLanderAgent.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using Lander.Thrusters;
+using Lander.CraftState;
+using Lander.Control;
+using Lander.ProximitySensors;
+
+namespace Lander.AI
+{
+
+    /// <summary>Агент ИИ для посадочного модуля.</summary>
+    public class AutoLanderAgent : Agent
+	{
+        [Header("Controls")]
+        [SerializeField]
+        private ProximitySensorsArray SensorsArray;
+
+        [SerializeField]
+        private ThrustersController ThrustersController;
+
+        [SerializeField]
+        private CraftManager CraftManager;
+
+        [SerializeField]
+        private Rigidbody lander;
+
+        [Header("Logging")]
+        [SerializeField]
+        private bool ShowRewardsLog;
+
+        [Header("Inital state")]
+        [SerializeField]
+        private Vector3 initialSpeed;
+
+        [SerializeField]
+        private Vector3 initialAngularSpeed;
+
+        [SerializeField]
+        private bool ShouldResetScene = true;
+
+        private Vector3 initialPos;
+        private Quaternion initialRotation;
+        private List<ProximitySensor> sensors;
+        private float? startHeight;
+
+
+
+        public override void Initialize()
+        {
+            Rewards.UseLogging = ShowRewardsLog || Rewards.UseLogging;
+            initialPos = lander.position;
+            initialRotation = lander.transform.rotation;
+            initialSpeed = lander.velocity;
+            initialAngularSpeed = lander.angularVelocity;
+        }
+
+        public override void CollectObservations(VectorSensor sensor)
+        {
+            var state = CraftManager.State;
+
+            // 3 + 3 + 3 + 1 + 1 + 5 = 16
+            sensor.AddObservation(state.Movement.Normal.x);
+            sensor.AddObservation(state.Movement.Normal.y);
+            sensor.AddObservation(state.Movement.Normal.z);
+            sensor.AddObservation(state.Movement.Velocity.x);
+            sensor.AddObservation(state.Movement.Velocity.y);
+            sensor.AddObservation(state.Movement.Velocity.z);
+            sensor.AddObservation(state.Movement.AngularVelocity.x);
+            sensor.AddObservation(state.Movement.AngularVelocity.y);
+            sensor.AddObservation(state.Movement.AngularVelocity.z);
+            sensor.AddObservation(ThrustersController.Fuel);
+            sensor.AddObservation(state.Movement.Height);
+            foreach (var s in SensorsArray.Sensors)
+                sensor.AddObservation(s.Distance);
+        }
+
+        public override void OnActionReceived(ActionBuffers actionBuffers)
+        {
+            float up = actionBuffers.ContinuousActions[0] > 0 ? actionBuffers.ContinuousActions[0] : 0;
+            float pitch = actionBuffers.ContinuousActions[1];
+            float yaw = actionBuffers.ContinuousActions[2];
+            float roll = actionBuffers.ContinuousActions[3];
+
+            ThrustersController.ApplyMovement(0, up, 0, pitch, yaw, roll);
+
+            if (CraftManager.State is FinalState)
+            {
+                if (CraftManager.State is FatalState || ThrustersController.Fuel == 0)
+                    AddReward(Rewards.FatalReward());
+                else if (CraftManager.State is LandedState)
+                    AddReward(Rewards.LandedReward());
+
+                EndEpisode();
+            }
+            else if (CraftManager.State is FlyingState)
+                AddReward(Rewards.FlyingReward(CraftManager.State.Movement));
+            else if (CraftManager.State is TouchedState)
+                AddReward(Rewards.FlyingReward(CraftManager.State.Movement));
+            else if (CraftManager.State is FixationState)
+                AddReward(Rewards.FixationReward(CraftManager.State.Movement));
+        }
+
+        public override void Heuristic(in ActionBuffers actionsOut)
+        {
+            var continuousActionsOut = actionsOut.ContinuousActions;
+            continuousActionsOut[0] = Input.GetAxis("Jump");
+            continuousActionsOut[1] = Input.GetAxis("Pitch");
+            continuousActionsOut[2] = Input.GetAxis("Yaw");
+            continuousActionsOut[3] = Input.GetAxis("Roll");
+        }
+
+        public override void OnEpisodeBegin()
+        {
+            startHeight = CraftManager.State.Movement.Height;
+            if (ShouldResetScene)
+            {
+                lander.transform.position = initialPos;
+                lander.velocity = initialSpeed;
+                lander.angularVelocity = initialAngularSpeed;
+                lander.transform.rotation = initialRotation;
+
+                ThrustersController.ResetFuel();
+                CraftManager.Reset();
+            }
+            base.OnEpisodeBegin();
+        }
+
+
+        #region Rewards
+        
+        #endregion
+    }
+}
---
+++ Assets/Lander/Scripts/AI/Rewards.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+using Lander.Shared;
+
+namespace Lander.AI
+{
+    public static class Rewards
+    {
+        public static bool UseLogging;
+
+
+        public static float inverseSquareZeroed(float x, float xRoot = 1.0f, float xOffset = 0.0f) =>
+            2 * (1 / (Mathf.Pow((x - xOffset) / xRoot, 2) + 1) - 0.5f);
+
+        
+
+        public static float FlyingReward(MovementInfo movement)
+        {
+            var r_n = Vector3.Dot(movement.Normal, -movement.Velocity.normalized)*3 - 2;            // r in [-5, 1]
+            var r_v = inverseSquareZeroed(movement.Velocity.magnitude, xRoot: 20);                  // r in [-1, 1]
+            var r_w = 2 * (inverseSquareZeroed(movement.AngularVelocity.magnitude, xRoot: 10) - 1); // r in [-5, 0]
+            var r_up = Vector3.Dot(movement.Normal, Vector3.up) * 3 - 2;                            // r in [-5, 1]
+            var r_down = (Mathf.Atan(-movement.Velocity.y) + 3 * inverseSquareZeroed(movement.Velocity.y, 4, -4)) / 4; // r in [-1, 1]
+            var r = r_n + r_v + r_w + r_up + r_down;                                                 // R in [-17, 4] 
+
+            if (UseLogging)
+                Debug.Log($"FlyingReward = r_n:{r_n:F1} + r_v:{r_v:F1} + r_w:{r_w:F1} + r_up:{r_up:F1}  + r_down:{r_down:F1} = {r:F2}");
+
+            return r;        
+        }
+
+        public static float FixationReward(MovementInfo movement)
+        {
+            var r_up = Vector3.Dot(movement.Normal, Vector3.up) * 3 - 2;                            // r in [-5, 1]
+            var r_v = inverseSquareZeroed(movement.Velocity.magnitude, xRoot: 0.5f);                // r in [-1, 1]
+            var r = r_v + r_up;                                                                     // R in [-6, 2] 
+
+            if (UseLogging)
+                Debug.Log($"FixationReward = r_v:{r_v:F1} + r_up:{r_up:F1} = {r:F2}");
+
+            return r;
+        }
+
+        public static float TouchedReward(MovementInfo movement)
+        {
+            var r_up = Vector3.Dot(movement.Normal, Vector3.up) * 3 - 2;                            // r in [-5, 1]
+            var r_v = inverseSquareZeroed(movement.Velocity.magnitude, xRoot: 5);                   // r in [-1, 1]
+            var r = r_v + r_up;                                                                     // R in [-6, 2]
+
+            if (UseLogging)
+                Debug.Log($"TouchedReward = r_v:{r_v:F1} + r_up:{r_up:F1} = {r:F2}");
+
+            return r;
+        }
+
+        public static float FatalReward()
+        {
+            var r = -1000;  
+            if (UseLogging)
+                Debug.Log($"FatalReward = {r:F2}");
+            return r;
+        }
+
+        public static float LandedReward()
+        {
+            var r = 1000;
+            if (UseLogging)
+                Debug.Log($"LandedReward = {r:F2}");
+            return r;
+        }
+    }
+}
