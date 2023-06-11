8a743e91bec3e46765feaa8022f88240bb37dcee
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2023-06-11 19:35:00 +0300

    Readonly property viewer

---
+++ Assets/Lander/Scripts/Shared/ReadOnlyDrawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace Lander.Shared
+{
+
+    /// <summary>
+    /// This class contain custom drawer for ReadOnly attribute.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ReadonlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        /// <summary>
+        /// Unity method for drawing GUI in Editor
+        /// </summary>
+        /// <param name="position">Position.</param>
+        /// <param name="property">Property.</param>
+        /// <param name="label">Label.</param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = true;
+        }
+    }
+
+
+    /// <summary>Атрибут для отображения свойства в редакторе без возможности редактирования.</summary>
+    public sealed class ReadonlyAttribute : PropertyAttribute
+    {
+    }
+}
